019bcb7a0da541b070613847adc036da7851aa57
Kadu Simoes <kadu_simoes_1@hotmail.com>
2017-12-06 00:20:47 -0200

    Change colors

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   resources :follows, only: [:create, :destroy]
 
   get '/profile/:id', to: "users#show", as: "user"
-  get '/profile/:id/followings', to: "users#followings", as: "following"
+  get '/profile/:id/followings', to: "users#followings", as: "followings"
   get '/profile/:id/followers', to: "users#followers", as: "followers"
   post '/users/search', to: "users#search", as: :search
   get '/users/search', to: "users#search"
-
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
